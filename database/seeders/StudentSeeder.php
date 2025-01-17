cf10eaf6056d17e69fbab265f5c52ffae037879f
MD ABDUR RAHAMAN <abdurrahaman444.spy.com>
2025-01-17 15:45:17 +0600

    run seeder with json using create method

--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\student;
+use Illuminate\Support\Facades\File;
 
 class StudentSeeder extends Seeder
 {
@@ -13,28 +14,16 @@ class StudentSeeder extends Seeder
      */
     public function run(): void
     {
-        $students= collect(
-            [
-                [
-                'name'=>'ya2hoo2 baba',
-                'email'=> 'yaa2hoobaba@gmail.com'
-                ],
-                [
-                'name'=>'Salman Khan',
-                'email'=> 'salman@gmail.com'
-                ],
-                [
-                'name'=>'Amitabh Bachan',
-                'email'=> 'amitabh@gmail.com'
-                ]
-            ]
-        );
+        $json=File::get(path:'database/json/students.json');
+        $students=collect(json_decode($json));
+
         $students->each(function ($student){
-                student::insert($student);
+                // student::insert($student);
+                student::create([
+                    'name'=>$student->name,
+                    'email'=> $student->email
+                ]);
         });
-        student::create([
-            'name'=>'yahoo2 baba',
-            'email'=> 'yahoo2baba@gmail.com'
-        ]);
+
     }
 }
